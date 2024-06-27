c18f43555212525bf436839b1573177478454de0
Ngeh Kellypride Cho <ngehkellypride181@gmail.com>
2024-06-27 11:25:18 +0100

    Modified data in signin and signup pages

--- src/pages/signin.jsx
+++ src/pages/signin.jsx
@@ -1,22 +1,24 @@
 import React from "react";
 import Input from "../components/input";
 import { GiBoltEye } from "react-icons/gi";
+import SignInImg from "../assets/signin.png";
 import Button from "../components/button";
 const SignIn = () => {
   return (
-    <div className=" flex flex-col gap-10 py-9 px-5 bg-slate-950 font-robot ">
-      <h1 className="font-bold text-2xl text-center text-gray-100 ">
-        Let's get started
-      </h1>
-      <div className="flex flex-col gap-44">
+    <div className="flex flex-col  gap-32 py-9 px-5 bg-slate-950 font-robot">
+      <div className="flex flex-col gap-2">
+        <div className="flex flex-col gap-x-4 items-center">
+          <h1 className="font-bold text-2xl text-center text-gray-100">
+            Welcome Back
+          </h1>
+          <img
+            src={SignInImg}
+            alt="signInImage"
+            className=" rounded-lg h-60 w-60 border-2 object-fit object-center border-cyan-200"
+          />
+        </div>
         <div className="flex flex-col gap-3">
-          <div className=" flex flex-col py-4 gap-6">
-            <Input
-              placeholder="Enter user name"
-              className=" bg-zinc-800 text-slate-200"
-              type="text"
-              // onChange={handleOnChange}
-            />
+          <div className=" flex flex-col py-4 gap-4">
             <Input
               placeholder="Email"
               className=" bg-zinc-800 text-slate-200"
@@ -31,28 +33,19 @@ const SignIn = () => {
               type="text"
               // onChange={handleOnChange}
             />
-            <Input
-              iconPosition="right"
-              icon={<GiBoltEye size={48} color={"#cbd5e1"} />}
-              placeholder="Confirm Password"
-              className=" bg-zinc-800 text-slate-200"
-              type="text"
-              // onChange={handleOnChange}
-            />
           </div>
           <p className="text-right text-xs">
             <span className="text-gray-100 font-light">
-              Already have an account?
+              Forgotten password?
             </span>
-            <span className="text-cyan-200">Sign In</span>
+            <span className="text-cyan-200">Resset password</span>
           </p>
         </div>
-
-        <Button
-          text="Sign Up"
-          className="py-4 px-4 bg-cyan-200 text-gray-800 rounded-full "
-        />
       </div>
+      <Button
+        text="Sign In"
+        className="py-4 px-4 bg-cyan-200 text-gray-800 rounded-full "
+      />
     </div>
   );
 };
--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,25 +1,23 @@
 import React from "react";
-import SignInImg from "../assets/signin.png";
 import Input from "../components/input";
 import { GiBoltEye } from "react-icons/gi";
 import Button from "../components/button";
 
 const SignUp = () => {
   return (
-    <div className="flex flex-col  gap-32 py-9 px-5 bg-slate-950 font-robot">
-      <div className="flex flex-col gap-2">
-        <div className="flex flex-col gap-x-4 items-center">
-          <h1 className="font-bold text-2xl text-center text-gray-100">
-            Welcome Back
-          </h1>
-          <img
-            src={SignInImg}
-            alt="signInImage"
-            className=" rounded-lg h-60 w-60 border-2 object-fit object-center border-cyan-200"
-          />
-        </div>
+    <div className=" flex flex-col gap-10 py-9 px-5 bg-slate-950 font-robot ">
+      <h1 className="font-bold text-2xl text-center text-gray-100 ">
+        Let's get started
+      </h1>
+      <div className="flex flex-col gap-44">
         <div className="flex flex-col gap-3">
-          <div className=" flex flex-col py-4 gap-4">
+          <div className=" flex flex-col py-4 gap-6">
+            <Input
+              placeholder="Enter user name"
+              className=" bg-zinc-800 text-slate-200"
+              type="text"
+              // onChange={handleOnChange}
+            />
             <Input
               placeholder="Email"
               className=" bg-zinc-800 text-slate-200"
@@ -34,19 +32,28 @@ const SignUp = () => {
               type="text"
               // onChange={handleOnChange}
             />
+            <Input
+              iconPosition="right"
+              icon={<GiBoltEye size={48} color={"#cbd5e1"} />}
+              placeholder="Confirm Password"
+              className=" bg-zinc-800 text-slate-200"
+              type="text"
+              // onChange={handleOnChange}
+            />
           </div>
           <p className="text-right text-xs">
             <span className="text-gray-100 font-light">
-              Forgotten password?
+              Already have an account?
             </span>
-            <span className="text-cyan-200">Resset password</span>
+            <span className="text-cyan-200">Sign In</span>
           </p>
         </div>
+
+        <Button
+          text="Sign Up"
+          className="py-4 px-4 bg-cyan-200 text-gray-800 rounded-full "
+        />
       </div>
-      <Button
-        text="Sign Up"
-        className="py-4 px-4 bg-cyan-200 text-gray-800 rounded-full "
-      />
     </div>
   );
 };
